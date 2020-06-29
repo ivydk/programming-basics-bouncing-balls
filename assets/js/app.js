fd7a57e4d865c1ca3c0c838ec33d6e667f3277ad
Rimmert Zelle <rimmertzelle@192.168.68.116>
2020-06-29 16:01:12 +0200

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+console.log("JavaScript is loaded");
+
+// properties (the data) of a ball
+let colour = "orange";
+const radius = 10;
+let xVelocity = 5; // the speed of a ball on the x-axis
+let yVelocity = 6; // the speed of a ball on the y-axis
+
+createNewBall(radius, colour, xVelocity, yVelocity);

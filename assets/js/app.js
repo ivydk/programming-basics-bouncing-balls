e33fff3152ea43ea6fb71492b12ef982086d9266
ivydk <dekk0123@hz.nl>
2021-09-24 08:51:19 +0200

    java aangepast

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,8 +2,18 @@ console.log("JavaScript is loaded");
 
 // properties (the data) of a ball
 let colour = "orange";
+let colour2 = "blue";
+let colour3 = "pink";
+let colour4 = "purple";
 const radius = 10;
 let xVelocity = 5; // the speed of a ball on the x-axis
 let yVelocity = 6; // the speed of a ball on the y-axis
 
-createNewBall(radius, colour, xVelocity, yVelocity);
+createNewBall(radius, colour, xVelocity, yVelocity);;
+createNewBall(radius, colour2, xVelocity, yVelocity);
+createNewBall(radius, colour3, xVelocity, yVelocity);
+createNewBall(radius, colour4, xVelocity, yVelocity);
+createNewBall(radius, colour, xVelocity, yVelocity);;
+createNewBall(radius, colour2, xVelocity, yVelocity);
+createNewBall(radius, colour3, xVelocity, yVelocity);
+createNewBall(radius, colour4, xVelocity, yVelocity);

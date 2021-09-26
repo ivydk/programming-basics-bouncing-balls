62c909009d0be2407dcc21210a51367442eac510
ivydk <dekk0123@hz.nl>
2021-09-26 20:32:53 +0200

    na feedback verandert en promt gebruikt

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,45 @@
 console.log("JavaScript is loaded");
 
 // properties (the data) of a ball
-let colour = "orange";
-let colour2 = "blue";
-let colour3 = "pink";
-let colour4 = "purple";
+var color = prompt("What color would you like?");
 const radius = 10;
 let xVelocity = 5; // the speed of a ball on the x-axis
 let yVelocity = 6; // the speed of a ball on the y-axis
 
-createNewBall(radius, colour, xVelocity, yVelocity);;
-createNewBall(radius, colour2, xVelocity, yVelocity);
-createNewBall(radius, colour3, xVelocity, yVelocity);
-createNewBall(radius, colour4, xVelocity, yVelocity);
-createNewBall(radius, colour, xVelocity, yVelocity);;
-createNewBall(radius, colour2, xVelocity, yVelocity);
-createNewBall(radius, colour3, xVelocity, yVelocity);
-createNewBall(radius, colour4, xVelocity, yVelocity);
+// Je kan kiezen uit: blue, orange, yellow, red, pink, purple, green, black
+
+function colorBouncingBall() {
+    switch (color) {
+        case "blue":
+            color = "blue";
+            break;
+        case "orange":
+            color = "orange";
+            break;
+        case "yellow":
+            color = "yellow";
+            break;
+        case "red":
+            color = "red";
+            break;
+        case "pink":
+            color = "pink";
+            break;
+        case "purple":
+            color = "purple";
+            break;
+        case "purple":
+            color = "purple";
+            break;
+        case "green":
+            color = "green";
+            break;
+        case "black":
+            color = "black";
+            break;
+        default:
+            text = "I have never heard of that color";
+    }
+}
+
+createNewBall(radius, color, xVelocity, yVelocity);

32dbad6c2ec5b9fd23dc5a0a363d6a0e1e86cec9
ivydk <dekk0123@hz.nl>
2021-09-27 10:43:12 +0200

    gedeelte weggehaald wat nie nodig was

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,40 +6,4 @@ const radius = 10;
 let xVelocity = 5; // the speed of a ball on the x-axis
 let yVelocity = 6; // the speed of a ball on the y-axis
 
-// Je kan kiezen uit: blue, orange, yellow, red, pink, purple, green, black
-
-function colorBouncingBall() {
-    switch (color) {
-        case "blue":
-            color = "blue";
-            break;
-        case "orange":
-            color = "orange";
-            break;
-        case "yellow":
-            color = "yellow";
-            break;
-        case "red":
-            color = "red";
-            break;
-        case "pink":
-            color = "pink";
-            break;
-        case "purple":
-            color = "purple";
-            break;
-        case "purple":
-            color = "purple";
-            break;
-        case "green":
-            color = "green";
-            break;
-        case "black":
-            color = "black";
-            break;
-        default:
-            text = "I have never heard of that color";
-    }
-}
-
 createNewBall(radius, color, xVelocity, yVelocity);
